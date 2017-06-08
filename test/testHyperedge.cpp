5a6ae5c74a256a62a213033c76a9f00af78ea416
Moritz Schilling <moritz.schilling@dfki.de>
2017-06-08 11:26:36 +0200

    Changed the from link of HEDGE 1
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- test/testHyperedge.cpp
+++ test/testHyperedge.cpp
@@ -32,12 +32,14 @@ int main(void)
         std::cout << edgeId << std::endl;
     }
 
-    std::cout << "> Connect edges 1 -> 2" << std::endl;
+    std::cout << "> Connect edge 1 -> 2" << std::endl;
     assert(testGraph.to(1,2) == true);
     std::cout << "> Connect edges 2 -> 23" << std::endl;
     assert(testGraph.to(2,23) == true);
     std::cout << "> Connect edges 23 -> 24 (should fail)" << std::endl;
     assert(testGraph.to(23,24) == false);
+    std::cout << "> Connect edge 1 <- 23" << std::endl;
+    assert(testGraph.from(23,1) == true);
     std::cout << "> Make a traversal starting at 1" << std::endl;
     id = testGraph.traversal(
             1,
