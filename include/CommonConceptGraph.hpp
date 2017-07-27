860b8a0d7af45bb4df77b4bcc4cafd47df50fd7e
Moritz Schilling <moritz.schilling@dfki.de>
2017-07-27 17:56:29 +0200

    Introduce the new Common Concept Graph
    This class is used to introduce some very important, relational concepts.
    It shall serve as a basis for all derived modeling domains to ensure that
    most knowledge is encoded in a similar way.
    
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

---
+++ include/CommonConceptGraph.hpp
@@ -0,0 +1,97 @@
+#ifndef _COMMON_CONCEPTGRAPH_HPP
+#define _COMMON_CONCEPTGRAPH_HPP
+
+#include "Conceptgraph.hpp"
+
+/*
+* This concept graph introduces the well known concepts of subsumption, composition etc.
+* The following relation ensure a sound architecture of these concepts.
+*
+* SUBREL-OF     Encodes that some relation is a subrelation of some relation superclass (subsumption)
+* IS-A          Encodes that some concept is a subclass of some concept class (subsumption)
+* INSTANCE-OF   Encodes a concept being an instance of a concept class
+* HAS-A         Encodes aggregation
+* PART-OF       Encodes composition
+* CONNECTS      Encodes topology
+*
+* To support the concept of relation classes and relation instances the following meta-relation is needed.
+* However, it can not be easily encoded as the ones before, because it is somewhat ouside the simple relation world.
+*
+* FACT-OF       Encodes a relation being an instance of a relation class
+*
+* Consider now the following:
+*
+* FACT-OF <-- FACT-OF --> C[FACT-OF]
+*             ^-- FACT-OF --^   ^
+*                 ^-- FACT-OF --|
+*
+* Fortunately, we can encode this infinite loop! We do this:
+*
+* FACT-OF <-- FACT-OF --> C[FACT-OF]
+*             ^--|
+*
+* That means that the POINTING FROM set of the FACT-OF in the middle points to itself!!
+* Only ONE C[FACT-OF] and ONE FACT-OF with a loop is needed to encode all other FACT-OF relations.
+* Note also, that we do it like this such that C[FACT-OF] can point from RELATION metaclass to RELATION metaclass which is sound.
+* If we'd relax this, we could also do:
+*
+* FACT-OF <-- C[FACT-OF]
+*             ^--|  |--^
+*
+* If we also pull C[FACT-OF] outside of the relation domain (e.g. as a third meta-model besides CONCEPT & RELATION) we could do:
+*
+* FACT-OF <-- C[FACT-OF]
+*
+* Currently, the second solution has been chosen, because it nicely fits into the picture of the class pointing to the instances.
+*
+* Apart from the common relations introduced here, there are convenience functions based on them which will ease the interpretation
+* of graphs build from them.
+* 
+*/
+
+class CommonConceptGraph : public Conceptgraph
+{
+    public:
+        /*As stated above this HEDGE will point FROM all RELATIONS which encode FACT-OF relations. It also points FROM AND TO itself!!!*/
+        static const unsigned FactOfId;
+        /*This relation cannot be subclassed by itself!*/
+        static const unsigned SubrelOfId;
+
+        /*Other predefined relations*/
+        static const unsigned IsAId;
+        static const unsigned HasAId;
+        static const unsigned PartOfId;
+        static const unsigned ConnectsId;
+        static const unsigned InstanceOfId;
+
+        void createCommonConcepts();
+
+        /*Constructors*/
+        CommonConceptGraph();
+        CommonConceptGraph(Conceptgraph& base);
+
+        /*Make facts*/
+        unsigned factOf(const unsigned factId, const unsigned superRelId);
+        unsigned relateFrom(const unsigned fromId, const unsigned toId, const unsigned superId);
+
+        /*Constructive functions using predefined relations*/
+        unsigned subrelationOf(const unsigned subRelId, const unsigned superRelId);
+        unsigned isA(const unsigned subId, const unsigned superId);
+        unsigned hasA(const unsigned parentId, const unsigned childId);
+        unsigned partOf(const unsigned partId, const unsigned wholeId);
+        unsigned connects(const unsigned connectorId, const unsigned interfaceId);
+        unsigned instanceOf(const unsigned individualId, const unsigned superId);
+
+        /*Common queries*/
+        Hyperedges factsOf(const unsigned superRelId, const std::string& label="");        //non-transitive
+        Hyperedges subrelationsOf(const unsigned superRelId, const std::string& label=""); //transitive
+
+        /*Other common queries using subrelationsOf!*/
+        Hyperedges transitiveClosure(const unsigned rootId, const unsigned relId, const std::string& label="", const TraversalDirection dir=DOWN);
+        Hyperedges subclassesOf(const unsigned superId, const std::string& label="");      //transitive
+        Hyperedges partsOf(const unsigned wholeId, const std::string& label="");           //transitive
+        Hyperedges instancesOf(const unsigned superId, const std::string& label="");       //non-transitive
+        Hyperedges childrenOf(const unsigned parentId, const std::string& label="");       //non-transitive
+};
+
+#endif
---
+++ src/CommonConceptGraph.cpp
@@ -0,0 +1,196 @@
+#include "CommonConceptGraph.hpp"
+//#include <iostream>
+
+const unsigned CommonConceptGraph::FactOfId = 3;
+const unsigned CommonConceptGraph::SubrelOfId = 4;
+const unsigned CommonConceptGraph::IsAId = 5;
+const unsigned CommonConceptGraph::HasAId = 6;
+const unsigned CommonConceptGraph::PartOfId = 7;
+const unsigned CommonConceptGraph::ConnectsId = 8;
+const unsigned CommonConceptGraph::InstanceOfId = 9;
+
+void CommonConceptGraph::createCommonConcepts()
+{
+    /* Common relations over relations*/
+    Conceptgraph::relate(CommonConceptGraph::FactOfId, Conceptgraph::RelationId, Conceptgraph::RelationId, "FACT-OF");
+    Conceptgraph::relate(CommonConceptGraph::SubrelOfId, Conceptgraph::RelationId, Conceptgraph::RelationId, "SUBREL-OF");
+
+    /* Common relations over concepts*/
+    Conceptgraph::relate(CommonConceptGraph::IsAId, Conceptgraph::ConceptId, Conceptgraph::ConceptId, "IS-A");
+    Conceptgraph::relate(CommonConceptGraph::HasAId, Conceptgraph::ConceptId, Conceptgraph::ConceptId, "HAS-A");
+    Conceptgraph::relate(CommonConceptGraph::PartOfId, Conceptgraph::ConceptId, Conceptgraph::ConceptId, "PART-OF");
+    Conceptgraph::relate(CommonConceptGraph::ConnectsId, Conceptgraph::ConceptId, Conceptgraph::ConceptId, "CONNECTS");
+    Conceptgraph::relate(CommonConceptGraph::InstanceOfId, Conceptgraph::ConceptId, Conceptgraph::ConceptId, "INSTANCE-OF");
+
+    // Make factOf point from and to itself
+    get(CommonConceptGraph::FactOfId)->from(CommonConceptGraph::FactOfId);
+    get(CommonConceptGraph::FactOfId)->to(CommonConceptGraph::FactOfId);
+}
+
+CommonConceptGraph::CommonConceptGraph()
+: Conceptgraph()
+{
+    createCommonConcepts();
+}
+
+CommonConceptGraph::CommonConceptGraph(Conceptgraph& A)
+: Conceptgraph(A)
+{
+    createCommonConcepts();
+}
+
+unsigned CommonConceptGraph::factOf(const unsigned factId, const unsigned superRelId)
+{
+    // Is it as simple as this?
+    // TODO:
+    // * Currently, relate can fail giving id=0 ... in this case that means that there already exists a factOf relation between factId and superRelId?
+    unsigned id = 0;
+    if (Conceptgraph::relations().count(factId) && Conceptgraph::relations().count(superRelId))
+    {
+        id = Conceptgraph::relate(factId, superRelId, CommonConceptGraph::FactOfId);
+        if (id)
+        {
+            // If we have created a factOf a factOf superrelation :D , we have to link them
+            get(CommonConceptGraph::FactOfId)->from(id);
+        }
+    }
+    return id;
+}
+
+unsigned CommonConceptGraph::relateFrom(const unsigned fromId, const unsigned toId, const unsigned superId)
+{
+    // At first create the relation ...
+    unsigned id = Conceptgraph::relate(fromId, toId, superId);
+    if (id)
+    {
+        // ... then make the new relation a factOf the super relation
+        factOf(id, superId);
+    }
+    return id;
+}
+
+unsigned CommonConceptGraph::subrelationOf(const unsigned subRelId, const unsigned superRelId)
+{
+    unsigned id = 0;
+    if (Conceptgraph::relations().count(subRelId) && Conceptgraph::relations().count(superRelId))
+    {
+        id = relateFrom(subRelId, superRelId, CommonConceptGraph::SubrelOfId);
+    }
+    return id;
+}
+
+unsigned CommonConceptGraph::isA(const unsigned subId, const unsigned superId)
+{
+    unsigned id = 0;
+    if (Conceptgraph::find().count(subId) && Conceptgraph::find().count(superId))
+    {
+        id = relateFrom(subId, superId, CommonConceptGraph::IsAId);
+    }
+    return id;
+}
+
+unsigned CommonConceptGraph::hasA(const unsigned parentId, const unsigned childId)
+{
+    unsigned id = 0;
+    if (Conceptgraph::find().count(parentId) && Conceptgraph::find().count(childId))
+    {
+        id = relateFrom(parentId, childId, CommonConceptGraph::HasAId);
+    }
+    return id;
+}
+unsigned CommonConceptGraph::partOf(const unsigned partId, const unsigned wholeId)
+{
+    unsigned id = 0;
+    if (Conceptgraph::find().count(partId) && Conceptgraph::find().count(wholeId))
+    {
+        id = relateFrom(partId, wholeId, CommonConceptGraph::PartOfId);
+    }
+    return id;
+}
+unsigned CommonConceptGraph::connects(const unsigned connectorId, const unsigned interfaceId)
+{
+    unsigned id = 0;
+    if (Conceptgraph::find().count(connectorId) && Conceptgraph::find().count(interfaceId))
+    {
+        id = relateFrom(connectorId, interfaceId, CommonConceptGraph::ConnectsId);
+    }
+    return id;
+}
+unsigned CommonConceptGraph::instanceOf(const unsigned individualId, const unsigned superId)
+{
+    unsigned id = 0;
+    if (Conceptgraph::find().count(individualId) && Conceptgraph::find().count(superId))
+    {
+        id = relateFrom(individualId, superId, CommonConceptGraph::InstanceOfId);
+    }
+    return id;
+}
+
+Hypergraph::Hyperedges CommonConceptGraph::factsOf(const unsigned superRelId, const std::string& label)
+{
+    Hyperedges result;
+    Hyperedges all = Conceptgraph::relationsOf(superRelId, get(CommonConceptGraph::FactOfId)->label());
+    // NOTE: all contains (FACT-OF --> superRelId) && (superRelId <-- FACT-OF)
+    for (unsigned id : all)
+    {
+        // Here we filter out (superRelId <-- FACT-OF)
+        if (Hypergraph::get(id)->isPointingFrom(superRelId))
+            continue;
+
+        // Here we filter out the hedges in the from set not matching the label
+        Hyperedges matches = Hypergraph::from(id, label);
+        result.insert(matches.begin(), matches.end());
+    }
+    return result;
+}
+
+Hypergraph::Hyperedges CommonConceptGraph::subrelationsOf(const unsigned superRelId, const std::string& label)
+{
+    // Here we start a traversal from superRelId following every subrelationOf relation
+    // Since this cannot be subclassed/relabelled, we can use a simple traverse function
+    return Conceptgraph::traverse(superRelId, label, get(CommonConceptGraph::SubrelOfId)->label(), UP);
+}
+
+Hypergraph::Hyperedges CommonConceptGraph::transitiveClosure(const unsigned rootId, const unsigned relId, const std::string& label, const TraversalDirection dir)
+{
+    // At first, find all relations we have to consider during traversal:
+    // These are all subrelations of relId including relId itself
+    Hyperedges relationsToFollow = subrelationsOf(relId);
+    std::vector<std::string> relLabels;
+    for (unsigned id : relationsToFollow)
+    {
+        if (!get(id)->label().empty())
+            relLabels.push_back(get(id)->label());
+    }
+
+    // Then we have to traverse the hypergraph starting at rootId,
+    // checking label of visited hedges,
+    // and following every relation which has a label matching one of the relations to be followed
+    std::vector<std::string> visitedLabels;
+    if (!label.empty())
+        visitedLabels.push_back(label);
+    return Conceptgraph::traverse(rootId, visitedLabels, relLabels, dir);
+}
+
+Hypergraph::Hyperedges CommonConceptGraph::subclassesOf(const unsigned superId, const std::string& label)
+{
+    return transitiveClosure(superId, CommonConceptGraph::IsAId, label, UP);
+}
+
+Hypergraph::Hyperedges CommonConceptGraph::partsOf(const unsigned wholeId, const std::string& label)
+{
+    Hyperedges result;
+    return result;
+}
+
+Hypergraph::Hyperedges CommonConceptGraph::instancesOf(const unsigned superId, const std::string& label)
+{
+    Hyperedges result;
+    return result;
+}
+
+Hypergraph::Hyperedges CommonConceptGraph::childrenOf(const unsigned parentId, const std::string& label)
+{
+    Hyperedges result;
+    return result;
+}
