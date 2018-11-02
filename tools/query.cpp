e39d2e5cf5863f0997c2bfe7fbdd18a26ebbc957
Moritz Schilling <moritz.schilling@dfki.de>
2018-11-02 14:07:13 +0100

    Made first version of very generic query CLI tool
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- tools/query.cpp
+++ tools/query.cpp
@@ -1,4 +1,4 @@
-#include "CommonConceptGraph.hpp"
+#include "Hypergraph.hpp"
 #include "HypergraphYAML.hpp"
 
 #include <fstream>
@@ -10,29 +10,27 @@
 
 static struct option long_options[] = {
     {"help", no_argument, 0, 'h'},
-    {"uid", required_argument, 0, 'u'},
-    {"label", required_argument, 0, 'l'},
+    {"all", no_argument, 0, 'a'},
     {0,0,0,0}
 };
 
 void usage (const char *myName)
 {
-    std::cout << "Query a common concept graph\n";
+    std::cout << "Query a hypergraph using another one\n";
     std::cout << "Usage:\n";
-    std::cout << myName << " <yaml-file-in>\n\n";
+    std::cout << myName << " <yaml-file-in> <other-yaml-file>\n\n";
     std::cout << "Options:\n";
     std::cout << "--help\t" << "Show usage\n";
-    std::cout << "--uid=<uid>\t" << "Specify the hyperedge to be queried by UID\n";
-    std::cout << "--label=<label>\t" << "Specify the hyperedge(s) to be queried by label\n";
+    std::cout << "--all\t" << "Finds all occurrences of query graph in data graph\n";
     std::cout << "\nExample:\n";
-    std::cout << myName << " hypergraph.yml\n";
+    std::cout << myName << " datagraph.yml querygraph.yml\n";
 }
 
 int main (int argc, char **argv)
 {
-    UniqueId uid;
-    std::string label;
-    std::ofstream fout;
+    bool find_all = false;
+
+    std::cout << "Query a data hypergraph using a query hypergraph and subgraph isomorphism algorithm\n";
 
     // Parse command line
     int c;
@@ -45,40 +43,64 @@ int main (int argc, char **argv)
 
         switch (c)
         {
-            case 'u':
-                uid=std::string(optarg);
-                break;
-            case 'l':
-                label=std::string(optarg);
+            case 'a':
+                find_all = true;
                 break;
             case 'h':
             case '?':
                 break;
             default:
                 std::cout << "W00t?!\n";
-                return 1;
+                return -1;
         }
     }
 
-    if ((argc - optind) < 1)
+    if ((argc - optind) < 2)
     {
         usage(argv[0]);
-        return 1;
+        return -1;
     }
-    std::string fileNameIn(argv[optind]);
+    const std::string& fileNameIn(argv[optind]);
+    const std::string& fileNameIn2(argv[optind+1]);
 
     // Load graph
-    Hypergraph hypergraph(YAML::LoadFile(fileNameIn).as<Hypergraph>());
-    CommonConceptGraph ccgraph(hypergraph);
+    Hypergraph datagraph(YAML::LoadFile(fileNameIn).as<Hypergraph>());
+    Hypergraph querygraph(YAML::LoadFile(fileNameIn2).as<Hypergraph>());
 
-    Hyperedges candidateUids(ccgraph.find(label));
-    if (!uid.empty())
-        candidateUids = intersect(candidateUids, Hyperedges{uid});
+    // Find matching(s)
+    std::cout << "Searching ...\n";
+    unsigned int no_matches = 0;
+    std::stack< Mapping > searchSpace;
+    Mapping mapping(datagraph.match(querygraph, searchSpace, Hypergraph::defaultMatchFunc));
 
-    for (const UniqueId& candidateUid : candidateUids)
+    if (!mapping.size())
     {
-        std::cout << *ccgraph.get(candidateUid) << std::endl;
+        std::cout << "No match found\n";
+        return 0;
+    }
+    no_matches++;
+
+    // Dump first match
+    for (const auto &pair : mapping)
+    {
+        std::cout << querygraph.read(pair.first) << " -> " << datagraph.read(pair.second) << "\n";
     }
 
-    return 0;
+    // Dump other matches if desired
+    if (find_all)
+    {
+        while ((mapping = datagraph.match(querygraph, searchSpace, Hypergraph::defaultMatchFunc)).size())
+        {
+            std::cout << "\n";
+            for (const auto &pair : mapping)
+            {
+                std::cout << querygraph.read(pair.first) << " -> " << datagraph.read(pair.second) << "\n";
+            }
+            no_matches++;
+        }
+    }
+
+    std::cout << "\nFound " << no_matches << " matches\n";
+
+    return no_matches;
 }
