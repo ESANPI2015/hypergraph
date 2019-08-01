b9fd4e1ef23d18ccdb52f7147336918154484fa7
Moritz Schilling <moritz.schilling@dfki.de>
2019-08-01 15:47:23 +0200

    Use header-only catch2 for nice unit tests
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- test/testHypergraph.cpp
+++ test/testHypergraph.cpp
@@ -1,68 +1,47 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 #include "Hyperedge.hpp"
 #include "Hypergraph.hpp"
 #include "HypergraphYAML.hpp"
 
-#ifdef NDEBUG
-#undef NDEBUG
-#endif
-
-#include <fstream>
 #include <iostream>
-#include <cassert>
 
-int main(void)
+TEST_CASE("Construct an hypergraph", "[Hypergraph]")
 {
-    std::cout << "*** HYPERGRAPH TEST ***" << std::endl;
-
-    Hypergraph testGraph;
-
-    std::cout << "> Create edge" << std::endl;
-    assert(testGraph.create("1", "First edge").empty() == false);
-
-    std::cout << "> Create edge with desired id" << std::endl;
-    assert(testGraph.create("23", "Edge with id 23").empty() == false);
-
-    std::cout << "> Create edge" << std::endl;
-    assert(testGraph.create("2", "Second edge").empty() == false);
-
-    std::cout << "> All edges" << std::endl;
-    std::cout << testGraph.findByLabel() << std::endl;
-
-    std::cout << "> Connect edge 1 -> 2" << std::endl;
-    assert(testGraph.pointsTo(Hyperedges{"1"},Hyperedges{"2"}).size() == 2);
-    std::cout << "> Connect edges 2 -> 23" << std::endl;
-    assert(testGraph.pointsTo(Hyperedges{"2"},Hyperedges{"23"}).size() == 2);
-    std::cout << "> Connect edges 23 -> 24 (should fail)" << std::endl;
-    assert(testGraph.pointsTo(Hyperedges{"23"},Hyperedges{"24"}).size() == 0);
-    std::cout << "> Connect edge 23 <- 1" << std::endl;
-    assert(testGraph.pointsFrom(Hyperedges{"1"},Hyperedges{"23"}).size() == 2);
-    std::cout << "> Make a traversal starting at 1" << std::endl;
-    testGraph.traverse(
-            "1",
-            [](const Hypergraph& hg, const UniqueId& x, const Hyperedges& p){ std::cout << hg.access(x) << std::endl; return true; },
-            [](const Hypergraph& hg, const UniqueId& x, const UniqueId&y){ return true; }
-         );
-
-    std::cout << "> Store hypergraph using YAML" << std::endl;
-
-    std::ofstream fout;
-    fout.open("test.yml");
-    if(fout.good()) {
-        fout << YAML::StringFrom(testGraph) << std::endl;
-    } else {
-        std::cout << "FAILED\n";
+    Hypergraph hg;
+    REQUIRE(hg.exists(Hypergraph::Zero) == true);
+    REQUIRE(hg.size() == 1);
+    const Hyperedge invalid(hg.access("Non-existent"));
+    REQUIRE(invalid.id() == Hypergraph::Zero); 
+    REQUIRE(hg.create("1", "My first hedge").empty() == false);
+    REQUIRE(hg.access("1").id() == "1");
+    REQUIRE(hg.access("1").label() == "My first hedge");
+    REQUIRE(hg.create("2", "My second hedge").empty() == false);
+    REQUIRE(hg.findByLabel().size() == 3);
+    REQUIRE(hg.findByLabel("My first hedge").size() == 1);
+    REQUIRE(hg.pointsTo(Hyperedges{"1"}, Hyperedges{"2"}).size() == 2);
+    REQUIRE(hg.isPointingTo(Hyperedges{"1"}).size() == 1);
+    REQUIRE(hg.traverse(
+        "1",
+        [](const Hypergraph& hg, const UniqueId& x, const Hyperedges& p) -> bool { return true; },
+        [](const Hypergraph& hg, const UniqueId& x, const UniqueId& y) -> bool { return true; }
+    ) == Hyperedges{"1", "2"});
+    // TODO: Test pattern matching
+    SECTION("Pattern matching")
+    {
     }
-    fout.close();
-
-    std::cout << "> Create new hypergraph from YAML" << std::endl;
-
-    YAML::Node test(YAML::LoadFile("test.yml"));
-    Hypergraph restoredGraph(test.as<Hypergraph>());
-
-    std::cout << "> All edges of restored graph" << std::endl;
-    std::cout << restoredGraph.findByLabel() << std::endl;
-
-    std::cout << "*** TESTS DONE ***" << std::endl;
-
-    return 0;
+    // TODO: Test rewriting
+    SECTION("Rewriting")
+    {
+    }
+    SECTION("Serialize & Reconstruct")
+    {
+        const std::string& serialized(YAML::StringFrom(hg));
+        Hypergraph reconstructed(YAML::Load(serialized).as<Hypergraph>());
+        for (const UniqueId& a : hg.findByLabel())
+        {
+            REQUIRE(reconstructed.exists(a) == true);
+            // TODO: Check connections
+        }
+    }
 }
